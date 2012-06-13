1e34008d4f314a60a672406540eff8b7b86bd823
jinzhou <happy_kaidy@163.com>
2012-06-13 18:25:49 +0800

    add files

--- taobaoTracer/app/tracer.js
+++ taobaoTracer/app/tracer.js
@@ -30,14 +30,14 @@ function tracerWithjQuery(callback, safe){
 tracerWithjQuery(function($){
 	
 	var app_url = 'http://192.168.53.65:9090/mysuper/taobao/tracer/';
-	var specilChar = new RegExp('&', 'g');
 	
 	$('a', $('div[class="col-main"]')).bind('click', function(){
 		
-		alert($(this).attr('href')+'>>>>'+$(this).attr('href').replace(specilChar, '#'));
-		
-		data = 'infoName='+$(this).text()+'&infoUrl='+$(this).attr('href').replace(specilChar, '#')+'&timestamp='+$.now(); 
-		alert( encodeURI(data));
-		$.getJSON(app_url+'logTraceInfo.do', encodeURI(data));
+		var info = {};
+		info.infoName  = $(this).text();
+		info.infoUrl   = $.trim($(this).attr('href'));
+		info.timestamp = $.now(); 
+		 
+		$.post(app_url+'logTraceInfo.do', info);
 	});
 }, true);
